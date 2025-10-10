159d72de057e85feffb4f6386ba84f70c0d08eaa
Sehrish Azmat <163280134+SehrishAzmat@users.noreply.github.com>
2025-10-10 22:50:34 +0500

    Add files via upload

---
+++ QuizQuest2/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Signup from "./components/Signup";
+import LoginPage from "./components/LoginPage";
+import "./App.css";
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <div className="app-main">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+      <Footer />
+    </Router>
+  );
+}
+
+export default App;
